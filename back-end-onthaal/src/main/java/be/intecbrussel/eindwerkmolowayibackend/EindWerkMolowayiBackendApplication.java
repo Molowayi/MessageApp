773b1ad123a56f74bf52793917b10503795f62a0
Molo <molowayi@gmail.com>
2020-06-06 22:03:48 +0200

    Idem

--- back-end-onthaal/src/main/java/be/intecbrussel/eindwerkmolowayibackend/EindWerkMolowayiBackendApplication.java
+++ back-end-onthaal/src/main/java/be/intecbrussel/eindwerkmolowayibackend/EindWerkMolowayiBackendApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
@@ -26,6 +27,7 @@ import java.sql.Timestamp;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableJpaRepositories()
 public class EindWerkMolowayiBackendApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
