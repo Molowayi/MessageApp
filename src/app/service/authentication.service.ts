66c4501ce003a4ea4775e2eb187c8d6dc506b4bc
alexmolowayi <molowayi@gmail.com>
2020-07-08 22:17:38 +0200

    Refactored code in frontend

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -2,31 +2,48 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
+export class User{
+  constructor(
+    public status:string,
+     ) {}
+  
+}
+
+export class JwtResponse{
+  constructor(
+    public jwttoken:string,
+     ) {}
+  
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(
+    private httpClient:HttpClient
+  ) { 
+     }
 
-
-  authenticate(username, password) {
-    return this.httpClient.post<any>('http://localhost:8080/authenticate',{username,password}).pipe(
-     map(
-       userData => {
-        sessionStorage.setItem('username',username);
-        let tokenStr= 'Bearer '+userData.token;
-        sessionStorage.setItem('token', tokenStr);
-        return userData;
-       }
-     )
-
-    );
-  }
+     authenticate(username, password) {
+      return this.httpClient.post<any>('http://localhost:8080/authenticate',{username,password}).pipe(
+       map(
+         userData => {
+          sessionStorage.setItem('username',username);
+          let tokenStr= 'Bearer '+userData.token;
+          sessionStorage.setItem('token', tokenStr);
+          return userData;
+         }
+       )
+  
+      );
+    }
+  
 
   isUserLoggedIn() {
     let user = sessionStorage.getItem('username')
-    console.log(!(user === null))
+    //console.log(!(user === null))
     return !(user === null)
   }
 
@@ -34,4 +51,3 @@ export class AuthenticationService {
     sessionStorage.removeItem('username')
   }
 }
-
--- src/app/services/basic-auth-http-interceptor-service.service.ts
+++ src/app/services/basic-auth-http-interceptor-service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { AuthenticationService } from '../service/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BasicAuthHttpInterceptorServiceService {
+export class BasicAuthHtppInterceptorService implements HttpInterceptor {
 
   constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+
+    if (sessionStorage.getItem('username') && sessionStorage.getItem('token')) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: sessionStorage.getItem('token')
+        }
+      })
+    }
+
+    return next.handle(req);
+
+  }
 }
